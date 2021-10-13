966eacd540f0a8ee998540d96b5eea5d274aca9d
DmAwik <Awii15@mail.ru>
2021-10-13 10:34:03 +0300

    add Tests

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,13 +1,73 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using Fakers;
 
 namespace UnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTests
     {
+        private Faker faker = new Faker();
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestConstructorPropertyInitialization()
         {
+            var testValue = faker.Create<MyClass2>();
+            string name = "MyName";
+            Assert.AreEqual(testValue.Name, name);
+        }
+
+        [TestMethod]
+        public void TestValueType()
+        {
+            var testValue = faker.Create<int>();
+            Assert.IsInstanceOfType(testValue, typeof(int));
+        }
+
+        [TestMethod]
+        public void TestValueType2()
+        {
+            var testValue = faker.Create<List<string>>();
+            Assert.IsInstanceOfType(testValue, typeof(List<string>));
+        }
+
+        [TestMethod]
+        public void TestValueType3()
+        {
+            var testValue = faker.Create<List<List<bool>>>();
+            Assert.IsInstanceOfType(testValue, typeof(List<List<bool>>));
+        }
+
+        [TestMethod]
+        public void TestGenericType()
+        {
+            var testValue = faker.Create<List<int>>();
+
+            Assert.IsInstanceOfType(testValue, typeof(List<int>));
+            Assert.IsTrue(testValue.Count > 0);
+        }
+
+        [TestMethod]
+        public void TestNullValue()
+        {
+            var testValue = faker.Create<List<double>>();
+            Assert.IsNotNull(testValue);
+        }
+
+        public class MyClass2
+        {
+            private string name = "MyName";
+
+            public MyClass2()
+            {
+
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
         }
     }
 }
